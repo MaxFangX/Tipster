254726540b055d87fccc3fe211b93cf30a8d9263
Max Fang <maxfangx@gmail.com>
2016-07-16 00:51:42 -0700

    Model changes, migrations

--- tipster/models.py
+++ tipster/models.py
@@ -7,17 +7,34 @@ class Profile(models.Model):
     user = models.OneToOneField(User)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    upvote_balance = models.BigIntegerField()
+    upvote_balance = models.BigIntegerField(help_text="The balance from upvotes only")
+
+    def get_net_balance(self):
+        """
+        Returns the net balance of the account, taking deposits and
+        withdrawals into account
+        """
+        # TODO implement
+        pass
+
+    def recalculate_upvote_balance(self):
+        # Can insert validation checks here
+        pass
 
 
 # class Deposit(models.Model):
-#
+#     DEPOSIT_STATUS_CHOICES = (
+#         ('submitted', 'Submitted'),
+#         ('approved', 'Approved'),
+#         ('executed', 'Executed'),
+#     )
 #     profile = models.ForeignKey(Profile)
 #     created_at = models.DateTimeField(auto_now_add=True)
-#
-#
+#     modified_at = models.DateTimeField(auto_now=True)
+#     status = models.CharField(max_length=50, default='submitted', choices=DEPOSIT_STATUS_CHOICES)
+
+
 # class Withdrawal(models.Model):
-#
 #     profile = models.ForeignKey(Profile)
 #     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -34,7 +51,7 @@ class Post(models.Model):
     link = models.URLField()
     title = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=50, default='visible', blank=True, choices=POST_STATUS_CHOICES)
+    status = models.CharField(max_length=50, default='visible', choices=POST_STATUS_CHOICES)
 
 
 class Upvote(models.Model):
@@ -42,8 +59,26 @@ class Upvote(models.Model):
     user = models.ForeignKey(User)
     post = models.ForeignKey(Post)
     index = models.IntegerField()
+    amount = models.IntegerField()
+    has_disbursed = models.BooleanField()
 
     def save(self, *args, **kwargs):
-        if self.index < 0:
+        if self.amount < 0:
             raise Exception("Amount can't be less than 0")
+
+        created = True if not self.pk else False
+        if created:
+            self.has_disbursed = False
+
         super(Upvote, self).save(*args, **kwargs)
+
+        if created:
+            self.disburse_tip()
+
+    def disburse_tip(self):
+        if self.has_disbursed:
+            raise Exception("Already disbursed")
+
+        # TODO check net balance
+
+        # TODO implement
--- tipster/settings.py
+++ tipster/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,7 +112,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
